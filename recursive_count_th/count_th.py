0d7fd7aba106f7507b255265c7c65c0e07d5e2f0
Sam <Jeffcoat>
2019-09-13 11:21:15 -0500

    Recursion problem done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,23 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+##psudocode
+## We are going to be recieving a string
+## for our base case, we need to exit out our functions because it must contain 2 letters to work, in our case th
+#Evaluate the first two letters of the sting
+# if th is included in our first two letters:
+#   Call the function again (recursion), on the rest of the string minus the first letter, +1 to increment as you go 
+# Call the function again (recursion), on it minus our first letter 
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+        key= "th"
+
+        if len(word) <= 1:
+            return 0
+
+        if key in word[0:2]:
+            return 1 + count_th(word[1:])
+        else:
+            return count_th(word[1:])
